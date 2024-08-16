e2ee91f4d1fd5d6791fa8c343b4b922db72c000d
Jeroh1990 <jeronimoh1990@gmail.com>
2024-08-15 22:59:39 -0300

    crea el repositorio y sube todos los archivos

---
+++ README.md
@@ -0,0 +1,49 @@
+<h1 id="titulo" align="center">Encriptador de texto.</h1>
+ <p align="center">
+   <img id="insignia" src="https://img.shields.io/badge/STATUS-EN%20DESAROLLO-green">
+   </p>
+  
+<h3 id="indice">Indice</h3>
+
+*	[Titulo](#titulo)
+* [insignia](#insignia)
+*	[Índice](#indice)
+*	[Descripción del proyecto](#descripcion-del-proyecto)
+*	[Estado del proyecto](#estado-del-proyecto)
+*	[Demostración de funciones](#demostracion-de-funciones)
+*	[Acceso al proyecto](#acceso-al-proyecto)
+*	[Tecnologías utilizadas](#tecnologias-utilizadas)
+*	[Personas desarrolladoras del proyecto](#personas-desarrolladoras)
+* [Referencias](#referencias)
+* [conclusion](#conclusion)
+
+<h3 id="descripcion-del-proyecto">Descripción del proyecto:</h3>
+Este proyecto se realizo en el marco del curso ONE (Oracle Next Educacation) de las empresas Oracle y Alura Latam, en el cual fue solicitado crear un encriptador de textos, este resultado es la unión de todos los conceptos vistos en el curso de formación, incluye lógica de programación, HTML, CSS y JavaScript, usando programas como Visual Studio Core, Git Bash y la plataforma GitHub.
+
+<h3 id="estado-del-proyecto">Estado del proyecto:</h3> 
+El proyecto se encuentra en desarrollo, ya que considero que mi conocimiento todavía no es el mas amplio para dar por terminado este código tanto de HTML como de CSS o JavaScript, estoy seguro que debe tener muchas cosas por corregir o mejorar.
+
+<h3 id="demostracion-de-funciones">Demostración de funciones:</h3>
+
+<img src="https://github.com/Jerozh/encriptador-de-texto/blob/main/assets/Encriptador-de-texto.gif?raw=true" alt="demo"/>
+
+<h3 id="acceso-al-proyecto">Acceso al proyecto:</h3>
+Este proyecto es de acceso público.
+
+<h3 id="tecnologias-utilizadas">Tecnologías utilizadas:</h3> 
+En este proyecto se trabajó mayormente con el programa de Microsoft Visual Studio Core, en el cual se editó todo el código de HTML, CSS y JavaScript. Se utilizo GitBash para subir el proyecto a la plataforma de GitHub. En el código se utilizó tecnología SweetAlert 2 para crear unas alertas más atractivas. Se trabajo con Luri, la IA de Alura, ChatGpt y Leo IA del navegador Brave.
+
+<h3 id="personas-desarrolladoras">Personas desarrolladoras del proyecto:</h3>
+
+<img src="https://gcdnb.pbrd.co/images/5ztMLUxlRUgl.jpg" alt="yo" width="100"/>
+Mi nombre es Jerónimo Herrera, soy de la ciudad de Catamarca-Argentina y desarrolle este encriptador de texto. Espero sea el primero de muchos proyectos.
+
+<h3 id="referencias">Referencias</h3>
+https://www.w3schools.com/
+https://developer.mozilla.org/es/
+https://www.aluracursos.com/blog/como-escribir-un-readme-increible-en-tu-github
+
+<h3 id="conclusion">Conclusión:</h3>
+Para cerrar, fue un desafío de verdad llegar a terminar este proyecto, en el cual tuve que investigar mucho sobre funciones de CSS, de JavaScript, ver y rever los cursos de la plataforma Alura, integrar conceptos con la ayuda de las IA de Alura, ChatGpt y el navegador Brave.
+Me gustó mucho realizar esta actividad, lograr mi encriptador de texto, tengo mucho por aprender en el mundo de la programación, pero este fue un gran primer paso.
+Espero sea de utilidad el código, el encriptador.
---
+++ script.js
@@ -0,0 +1,121 @@
+//variables
+var cajaDerecha = document.getElementById('caja-texto2');
+var cajaIzquierda = document.getElementById('ingresee');
+var desbloquearBoton = document.getElementById('copiar');
+var infoDerecha = document.querySelector('.sector__derecho__error');
+var infoDerecha2 = document.querySelector('.sector__derecho__indicacion');
+
+//funcion que saca la imagen, los parrafos inferiores y muestra el boton copiar
+function quitarVariosYBotonCopiar(){
+  infoDerecha.style.visibility = 'hidden';
+  infoDerecha2.style.visibility = 'hidden';
+  cajaDerecha.style.backgroundImage = 'none';
+  desbloquearBoton.style.visibility = 'visible';  
+  cajaDerecha.style.display = 'inline';
+}
+//funcion que limpia la caja de texto derecha, y la vuelve a cero
+function resetCajaDerecha(){
+  cajaDerecha.value = '';
+  cajaDerecha.style.backgroundImage = 'url("../assets/Muñeco.png")';
+  cajaDerecha.style.position = 'center';
+  cajaDerecha.style.repeat = 'no-repeat';
+  infoDerecha.style.visibility = 'visible';
+  infoDerecha2.style.visibility = 'visible';
+  desbloquearBoton.style.visibility = 'hidden';
+}
+
+function limpiarCaja(){
+    cajaIzquierda.value = '';
+}
+//funcion que devuelve si hay mayusculas o acentos en el texto ingresado
+function mayusYAcentos(text) {
+  return /[A-Z]|[áéíóúÁÉÍÓÚÑ]/.test(text);
+}
+
+function botonEncriptar(){
+if(cajaIzquierda.value == ''){
+  resetCajaDerecha();  
+  swal.fire({
+    title: 'Error',
+    text: 'No hay texto para encriptar',
+    icon: 'error',
+    confirmButtonText: 'Aceptar'})
+    }
+else if (mayusYAcentos(cajaIzquierda.value) == false){
+var encriptarr = cajaIzquierda.value.replace(/e/g, 'enter').replace(/a/g, 'xjz').replace(/i/g, 'imes').replace(/o/g, 'ober').replace(/u/g, 'ufat');
+var encriptarr2 = encriptarr.replace(/xjz/g, 'ai');
+document.getElementById("caja-texto2").value = encriptarr2;
+swal.fire({
+  title: 'Logrado',
+  text: 'Su Texto se ha encriptado',
+  icon: 'success',
+  confirmButtonText: 'Aceptar'})
+quitarVariosYBotonCopiar()
+limpiarCaja();
+}
+else{
+   swal.fire({
+    title: 'Error',
+    text: 'Recorda que no podes ingresar palabras que contengan mayusculas, ni acentos',
+    icon: 'warning',
+    confirmButtonText: 'Aceptar'})
+  limpiarCaja();
+  resetCajaDerecha();
+ }
+}
+function botonDesencriptar(){
+if(cajaIzquierda.value == ''){
+  resetCajaDerecha();
+  swal.fire({
+    title: 'Error',
+    text: 'No hay texto para desencriptar',
+    icon: 'error',
+    confirmButtonText: 'Aceptar'
+  })  
+}
+else if(mayusYAcentos(cajaIzquierda.value) == false){
+var desencriptar = cajaIzquierda.value.replace(/enter/g, 'e').replace(/ai/g, 'xjz').replace(/imes/g, 'i').replace(/ober/g, 'o').replace(/ufat/g, 'u');
+var desencriptar2 = desencriptar.replace(/xjz/g, 'a');
+document.getElementById("caja-texto2").value = desencriptar2;
+swal.fire({
+  title: 'Logrado',
+  text: 'Su Texto se ha desencriptado',
+  icon: 'success',
+  confirmButtonText: 'Aceptar'})
+quitarVariosYBotonCopiar()
+limpiarCaja();
+}
+else{
+    swal.fire({
+    title: 'Error',
+    text: 'Recorda que no podes ingresar palabras que contengan mayusculas, ni acentos',
+    icon: 'warning',
+    confirmButtonText: 'Aceptar'})
+  limpiarCaja();
+  resetCajaDerecha();
+ }
+}
+
+function botonCopiar(){
+  let copiaTexto = cajaDerecha.value;
+  try {
+  navigator.clipboard.writeText(copiaTexto);
+    console.log('Texto copiado al portapapeles');
+    swal.fire({
+      title: 'Operacion completada',
+      text: 'Copia realizada',
+      icon: 'success',
+      confirmButtonText: 'Aceptar'})
+    
+  } catch (err) {
+    console.error('Falló la operación: ', err);
+  }
+  resetCajaDerecha();
+}
+
+/*valores para hacer la encriptacion
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
